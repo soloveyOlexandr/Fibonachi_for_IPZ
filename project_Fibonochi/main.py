c29aea8c555a577210403e3e87c1689441fe827f
soloveyOlexandr <120163436+soloveyOlexandr@users.noreply.github.com>
2024-09-22 20:10:25 +0300

    Add files via upload

---
+++ project_Fibonochi/main.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_fib = fib_sequence[i - 1] + fib_sequence[i - 2]
+        fib_sequence.append(next_fib)
+
+    return fib_sequence
+
+n = int(input("Введіть кількість чисел Фібоначчі: "))
+print(f"Перші {n} чисел Фібоначчі: {fibonacci(n)}")
